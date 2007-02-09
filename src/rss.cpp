6021c82fe5379e912b46c9f59a29df7c57c06e7f
Andreas Krennmair <ak@synflood.at>
2007-02-09 08:28:19 +0000

    Andreas Krennmair: 	oops, we need to check for content/content:encoded in the item, not the whole feed.

--- src/rss.cpp
+++ src/rss.cpp
@@ -51,25 +51,7 @@ rss_feed rss_parser::parse() {
 		}
 	}
 	
-	mrss_tag_t * content;
-
-	if (mrss_search_tag(mrss, "encoded", "http://purl.org/rss/1.0/modules/content/", &content) == MRSS_OK && content) {
-		/* RSS 2.0 content:encoded */
-		char * str = stringprep_convert(content->value, stringprep_locale_charset(), encoding);
-		if (str) {
-			feed.set_description(str);
-			free(str);
-		}
-		mrss_free(content);
-	} else if (mrss_search_tag(mrss, "content", NULL, &content) == MRSS_OK && content) {
-		/* Atom content */
-		char * str = stringprep_convert(content->value, stringprep_locale_charset(), encoding);
-		if (str) {
-			feed.set_description(str);
-			free(str);
-		}
-		mrss_free(content);
-	} else if (mrss->description) {
+	if (mrss->description) {
 		char * str = stringprep_convert(mrss->description, stringprep_locale_charset(), encoding);
 		if (str) {
 			feed.set_description(str);
@@ -96,7 +78,28 @@ rss_feed rss_parser::parse() {
 		}
 		if (item->link) x.set_link(item->link);
 		if (item->author) x.set_author(item->author);
-		if (item->description) {
+
+		mrss_tag_t * content;
+
+		if (mrss_search_tag(item, "encoded", "http://purl.org/rss/1.0/modules/content/", &content) == MRSS_OK && content) {
+			/* RSS 2.0 content:encoded */
+			GetLogger().log(LOG_DEBUG, "rss_parser::parse: found rss 2.0 content:encoded: %s\n", content->value);
+			char * str = stringprep_convert(content->value, stringprep_locale_charset(), encoding);
+			if (str) {
+				x.set_description(str);
+				free(str);
+			}
+		}
+		if (x.description().length() == 0 && mrss_search_tag(mrss, "content", NULL, &content) == MRSS_OK && content) {
+			/* Atom content */
+			GetLogger().log(LOG_DEBUG, "rss_parser::parse: found atom content: %s\n", content->value);
+			char * str = stringprep_convert(content->value, stringprep_locale_charset(), encoding);
+			if (str) {
+				x.set_description(str);
+				free(str);
+			}
+		}
+		if (x.description().length() == 0 && item->description) {
 			char * str = stringprep_convert(item->description,stringprep_locale_charset(), encoding);
 			if (str) {
 				x.set_description(str);
