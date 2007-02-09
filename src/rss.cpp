f067e829e57648d7ce3d9d24711ea3bfb1c8d4c4
Andreas Krennmair <ak@synflood.at>
2007-02-09 19:48:53 +0000

    Andreas Krennmair: 	fixed crash.

--- src/rss.cpp
+++ src/rss.cpp
@@ -84,19 +84,23 @@ rss_feed rss_parser::parse() {
 		if (mrss_search_tag(item, "encoded", "http://purl.org/rss/1.0/modules/content/", &content) == MRSS_OK && content) {
 			/* RSS 2.0 content:encoded */
 			GetLogger().log(LOG_DEBUG, "rss_parser::parse: found rss 2.0 content:encoded: %s\n", content->value);
-			char * str = stringprep_convert(content->value, stringprep_locale_charset(), encoding);
-			if (str) {
-				x.set_description(str);
-				free(str);
+			if (content->value) {
+				char * str = stringprep_convert(content->value, stringprep_locale_charset(), encoding);
+				if (str) {
+					x.set_description(str);
+					free(str);
+				}
 			}
 		}
 		if (x.description().length() == 0 && mrss_search_tag(mrss, "content", NULL, &content) == MRSS_OK && content) {
 			/* Atom content */
 			GetLogger().log(LOG_DEBUG, "rss_parser::parse: found atom content: %s\n", content->value);
-			char * str = stringprep_convert(content->value, stringprep_locale_charset(), encoding);
-			if (str) {
-				x.set_description(str);
-				free(str);
+			if (content->value) {
+				char * str = stringprep_convert(content->value, stringprep_locale_charset(), encoding);
+				if (str) {
+					x.set_description(str);
+					free(str);
+				}
 			}
 		}
 		if (x.description().length() == 0 && item->description) {
