0085e39c40ae3d34cabf2659ded148bbeecea187
Tanguy Kerdoncuff <tanguy@chalutier>
2016-10-28 21:06:18 +0200

    Open multiple items in browser
    Refactorizes the itemlist_formaction test class and skeleton
    feedlist_formaction test class

--- src/list_formaction.cpp
+++ src/list_formaction.cpp
@@ -8,18 +8,18 @@ list_formaction::list_formaction(view * v, std::string formstr)
 {}
 
 void list_formaction::open_unread_items_in_browser(std::shared_ptr<rss_feed> feed , bool markread){
-    int tabcount = 0;
-    for (auto item : feed->items()) {
-        if (tabcount < v->get_cfg()->get_configvalue_as_int("max-browser-tabs")) {
-            if (item->unread()) {
-                v->open_in_browser(item->link());
-                tabcount += 1;
-                item->set_unread(!markread);
-            }
-        } else {
-            break;
-        }
-    }
+	int tabcount = 0;
+	for (auto item : feed->items()) {
+		if (tabcount < v->get_cfg()->get_configvalue_as_int("max-browser-tabs")) {
+			if (item->unread()) {
+				v->open_in_browser(item->link());
+				tabcount += 1;
+				item->set_unread(!markread);
+			}
+		} else {
+			break;
+		}
+	}
 }
 
 }
--- test/itemlist_formaction.cpp
+++ test/itemlist_formaction.cpp
@@ -1,39 +1,33 @@
 #include "catch.hpp"
 
-#include "cache.h"
-#include "itemlist_formaction.h"
-#include "keymap.h"
-#include "stfl/itemlist.h"
+#include <cache.h>
+#include <itemlist_formaction.h>
+#include <keymap.h>
+#include <stfl/itemlist.h>
+#include "test-helpers.h"
 
 #include <unistd.h>
 
 using namespace newsbeuter;
 
 TEST_CASE("itemlist_formaction::process_op()") {
-
 	controller c;
 	newsbeuter::view v(&c);
-	std::string test_browser_file = "/tmp/test_browser.txt";
+	TestHelpers::TempFile browserfile;
+
 	std::string test_url = "http://test_url";
-	std::string cache_path = "test-cache.db";
 	std::string line;
 	int itemCount = 6;
-	int openedItemsCount = 0;
-	int lineCount = 0;
-	int maxItems = 4;
-	int openedItemsTarget = 0;
 
-	configcontainer * cfg = new configcontainer();
-	cfg->set_configvalue("browser", "echo %u >> " + test_browser_file);
-	cfg->set_configvalue("max-browser-tabs", std::to_string(maxItems));
+	std::shared_ptr<configcontainer> cfg ( new configcontainer());
+	cfg->set_configvalue("browser", "echo %u >> " + browserfile.getPath());
 
-	cache * rsscache = new cache(cache_path, cfg);
+	std::unique_ptr<cache> rsscache(new cache(":memory:", cfg.get()));
 
-	std::shared_ptr<rss_feed> feed(new rss_feed(rsscache));
+	std::shared_ptr<rss_feed> feed(new rss_feed(rsscache.get()));
 
-	for (int i = 0; i < itemCount; i++)
-	{
-		std::shared_ptr<rss_item> item(new rss_item(rsscache));
+	for (int i = 0; i < itemCount; i++) {
+		std::shared_ptr<rss_item> item(new rss_item(rsscache.get()));
 
 		item->set_link(test_url + std::to_string(i));
 		item->set_unread(true);
@@ -41,114 +35,96 @@ TEST_CASE("itemlist_formaction::process_op()") {
 		feed->add_item(item);
 	}
 
-	v.set_config_container(cfg);
+	v.set_config_container(cfg.get());
 	c.set_view(&v);
 
-	std::shared_ptr<itemlist_formaction> itemlist(new itemlist_formaction(&v, itemlist_str));
+	std::unique_ptr<itemlist_formaction> itemlist(new itemlist_formaction(&v, itemlist_str));
 	itemlist->set_feed(feed);
 
 	SECTION("Itemlist Formaction"){
 	
-		SECTION("Open all unread in browser, unread > max-browser-tabs"){
-			openedItemsTarget = maxItems;
+		SECTION("Open all unread in browser, unread >= max-browser-tabs"){
+			int maxItemsToOpen = 4;
+			int openedItemsCount = 0;
+			cfg->set_configvalue("max-browser-tabs", std::to_string(maxItemsToOpen));
+			
 			itemlist->process_op(OP_OPENALLUNREADINBROWSER);
 
-			std::ifstream myfile (test_browser_file);
+			std::ifstream browserFileStream (browserfile.getPath());
 			openedItemsCount = 0;
-  			if (myfile.is_open())
-  			{
-    				for (int i = 0; i < maxItems; i ++)
-    				{
-					if ( std::getline (myfile,line) )
-					{
+			if (browserFileStream.is_open()) {
+				for (int i = 0; i < maxItemsToOpen; i ++) {
+					if ( std::getline (browserFileStream,line) ) {
 						REQUIRE(line == test_url + std::to_string(i));
 						openedItemsCount += 1;
 					}
-    				}
-    				myfile.close();
+				}
+				browserFileStream.close();
 			}
-			REQUIRE(openedItemsCount == openedItemsTarget);
-			std::remove(test_browser_file.c_str());
+			REQUIRE(openedItemsCount == maxItemsToOpen);
 		}
 
 		SECTION("Open all unread in browser, unread < max-browser-tabs"){
-			maxItems = 9;
-			openedItemsCount = 0;
-			openedItemsTarget = itemCount;
-			cfg->set_configvalue("max-browser-tabs", std::to_string(maxItems));
+			int maxItemsToOpen = 9;
+			int openedItemsCount = 0;
+			cfg->set_configvalue("max-browser-tabs", std::to_string(maxItemsToOpen));
+
 			itemlist->process_op(OP_OPENALLUNREADINBROWSER);
 
-			std::ifstream myfile (test_browser_file);
-  			if (myfile.is_open())
-  			{
-    				for (int i = 0; i < itemCount; i ++)
-    				{
-					if ( std::getline (myfile,line) )
-					{
+			std::ifstream browserFileStream (browserfile.getPath());
+			if (browserFileStream.is_open()) {
+				for (int i = 0; i < itemCount; i ++) {
+					if ( std::getline (browserFileStream,line) ) {
 						REQUIRE(line == test_url + std::to_string(i));
 						openedItemsCount += 1;
 					}
-    				}
-    				myfile.close();
+				}
+				browserFileStream.close();
 			}
-			REQUIRE(openedItemsCount == openedItemsTarget);
-			std::remove(test_browser_file.c_str());
+			REQUIRE(openedItemsCount == itemCount);
 		}
 
-		SECTION("Open all unread in browser and mark read, unread > max-browser-tabs"){
-			maxItems = 4;
-			openedItemsCount = 0;
-			openedItemsTarget = maxItems;
-			cfg->set_configvalue("max-browser-tabs", std::to_string(maxItems));
+		SECTION("Open all unread in browser and mark read, unread >= max-browser-tabs"){
+			int maxItemsToOpen = 4;
+			int openedItemsCount = 0;
+			cfg->set_configvalue("max-browser-tabs", std::to_string(maxItemsToOpen));
+
 			itemlist->process_op(OP_OPENALLUNREADINBROWSER_AND_MARK);
 
-			std::ifstream myfile (test_browser_file);
-  			if (myfile.is_open())
-  			{
-    				for (int i = 0; i < itemCount; i ++)
-    				{
-					if ( std::getline (myfile,line) )
-					{
+			std::ifstream browserFileStream (browserfile.getPath());
+			if (browserFileStream.is_open()) {
+				for (int i = 0; i < itemCount; i ++) {
+					if ( std::getline (browserFileStream,line) ) {
 						REQUIRE(line == test_url + std::to_string(i));
 						openedItemsCount += 1;
 					}
-    				}
-    				myfile.close();
+				}
+				browserFileStream.close();
 			}
-			REQUIRE(openedItemsCount == openedItemsTarget);
-			REQUIRE(feed->unread_item_count() == itemCount - maxItems);
-			std::remove(test_browser_file.c_str());
+			REQUIRE(openedItemsCount == maxItemsToOpen);
+			REQUIRE(feed->unread_item_count() == itemCount - maxItemsToOpen);
 		}
 
 		SECTION("Open all unread in browser and mark read, unread < max-browser-tabs"){
-			openedItemsCount = 0;
-			openedItemsTarget = feed->unread_item_count();
-			maxItems = 9;
-			cfg->set_configvalue("max-browser-tabs", std::to_string(maxItems));
+			int maxItemsToOpen = 9;
+			int openedItemsCount = 0;
+			cfg->set_configvalue("max-browser-tabs", std::to_string(maxItemsToOpen));
+
 			itemlist->process_op(OP_OPENALLUNREADINBROWSER_AND_MARK);
 
-			std::ifstream myfile (test_browser_file);
-  			if (myfile.is_open())
-  			{
-    				for (int i = 0; i < itemCount; i ++)
-    				{
-					if ( std::getline (myfile,line) )
-					{
+			std::ifstream browserFileStream (browserfile.getPath());
+			if (browserFileStream.is_open()) {
+				for (int i = 0; i < itemCount; i ++) {
+					if ( std::getline (browserFileStream,line) ) {
 						REQUIRE(line == test_url + std::to_string(i));
 						openedItemsCount += 1;
 					}
-    				}
-    				myfile.close();
+				}
+				browserFileStream.close();
 			}
-			REQUIRE(openedItemsCount == openedItemsTarget);
+			REQUIRE(openedItemsCount == itemCount);
 			REQUIRE(feed->unread_item_count() == 0);
-			std::remove(test_browser_file.c_str());
 		}
 	}
 
-	delete cfg;
-	delete rsscache;
-
-
-	::unlink("test-cache.db");
 }
