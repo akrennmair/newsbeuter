2bd168696868b03854eb164dbcad7b5d1b3a5866
Tanguy Kerdoncuff <tanguy@chalutier>
2016-11-01 13:55:21 +0100

    Open multiple articles in browser
    Modified itemlist tests to fail in case of double article opening, and
    to accept unordered items opening.

--- test/itemlist_formaction.cpp
+++ test/itemlist_formaction.cpp
@@ -1,10 +1,10 @@
 #include "catch.hpp"
+#include "test-helpers.h"
 
 #include <cache.h>
 #include <itemlist_formaction.h>
 #include <keymap.h>
-#include <stfl/itemlist.h>
-#include "test-helpers.h"
+#include <itemlist.h>
 
 #include <unistd.h>
 
@@ -14,48 +14,50 @@ TEST_CASE("itemlist_formaction::process_op()") {
 	controller c;
 	newsbeuter::view v(&c);
 	TestHelpers::TempFile browserfile;
+	std::unordered_set<std::string> url_set;
 
 	std::string test_url = "http://test_url";
 	std::string line;
 	int itemCount = 6;
 
-	std::shared_ptr<configcontainer> cfg ( new configcontainer());
-	cfg->set_configvalue("browser", "echo %u >> " + browserfile.getPath());
+	configcontainer cfg;
+	cfg.set_configvalue("browser", "echo %u >> " + browserfile.getPath());
 
-	std::unique_ptr<cache> rsscache(new cache(":memory:", cfg.get()));
+	cache rsscache(":memory:", &cfg);
 
-	std::shared_ptr<rss_feed> feed(new rss_feed(rsscache.get()));
+	std::shared_ptr<rss_feed> feed(new rss_feed(&rsscache));
 
 	for (int i = 0; i < itemCount; i++) {
-		std::shared_ptr<rss_item> item(new rss_item(rsscache.get()));
-
+		std::shared_ptr<rss_item> item = std::make_shared<rss_item>(&rsscache);
 		item->set_link(test_url + std::to_string(i));
+		url_set.insert(test_url + std::to_string(i));
 		item->set_unread(true);
-
 		feed->add_item(item);
 	}
 
-	v.set_config_container(cfg.get());
+	v.set_config_container(&cfg);
 	c.set_view(&v);
 
-	std::unique_ptr<itemlist_formaction> itemlist(new itemlist_formaction(&v, itemlist_str));
-	itemlist->set_feed(feed);
+	itemlist_formaction itemlist(&v, itemlist_str);
+	itemlist.set_feed(feed);
 
 	SECTION("Itemlist Formaction"){
 	
 		SECTION("Open all unread in browser, unread >= max-browser-tabs"){
 			int maxItemsToOpen = 4;
 			int openedItemsCount = 0;
-			cfg->set_configvalue("max-browser-tabs", std::to_string(maxItemsToOpen));
-			
-			itemlist->process_op(OP_OPENALLUNREADINBROWSER);
+			cfg.set_configvalue("max-browser-tabs", std::to_string(maxItemsToOpen));
+
+			itemlist.process_op(OP_OPENALLUNREADINBROWSER);
 
 			std::ifstream browserFileStream (browserfile.getPath());
 			openedItemsCount = 0;
 			if (browserFileStream.is_open()) {
 				for (int i = 0; i < maxItemsToOpen; i ++) {
 					if ( std::getline (browserFileStream,line) ) {
-						REQUIRE(line == test_url + std::to_string(i));
+						INFO("Each URL should be present exactly once. Erase urls after first match to fail if an item opens twice.")
+						REQUIRE(url_set.count(test_url + std::to_string(i)) == 1);
+						url_set.erase(url_set.find(test_url + std::to_string(i)));
 						openedItemsCount += 1;
 					}
 				}
@@ -67,15 +69,17 @@ TEST_CASE("itemlist_formaction::process_op()") {
 		SECTION("Open all unread in browser, unread < max-browser-tabs"){
 			int maxItemsToOpen = 9;
 			int openedItemsCount = 0;
-			cfg->set_configvalue("max-browser-tabs", std::to_string(maxItemsToOpen));
+			cfg.set_configvalue("max-browser-tabs", std::to_string(maxItemsToOpen));
 
-			itemlist->process_op(OP_OPENALLUNREADINBROWSER);
+			itemlist.process_op(OP_OPENALLUNREADINBROWSER);
 
 			std::ifstream browserFileStream (browserfile.getPath());
 			if (browserFileStream.is_open()) {
 				for (int i = 0; i < itemCount; i ++) {
 					if ( std::getline (browserFileStream,line) ) {
-						REQUIRE(line == test_url + std::to_string(i));
+						INFO("Each URL should be present exactly once. Erase urls after first match to fail if an item opens twice.")
+						REQUIRE(url_set.count(test_url + std::to_string(i)) == 1);
+						url_set.erase(url_set.find(test_url + std::to_string(i)));
 						openedItemsCount += 1;
 					}
 				}
@@ -87,15 +91,17 @@ TEST_CASE("itemlist_formaction::process_op()") {
 		SECTION("Open all unread in browser and mark read, unread >= max-browser-tabs"){
 			int maxItemsToOpen = 4;
 			int openedItemsCount = 0;
-			cfg->set_configvalue("max-browser-tabs", std::to_string(maxItemsToOpen));
+			cfg.set_configvalue("max-browser-tabs", std::to_string(maxItemsToOpen));
 
-			itemlist->process_op(OP_OPENALLUNREADINBROWSER_AND_MARK);
+			itemlist.process_op(OP_OPENALLUNREADINBROWSER_AND_MARK);
 
 			std::ifstream browserFileStream (browserfile.getPath());
 			if (browserFileStream.is_open()) {
 				for (int i = 0; i < itemCount; i ++) {
 					if ( std::getline (browserFileStream,line) ) {
-						REQUIRE(line == test_url + std::to_string(i));
+						INFO("Each URL should be present exactly once. Erase urls after first match to fail if an item opens twice.")
+						REQUIRE(url_set.count(test_url + std::to_string(i)) == 1);
+						url_set.erase(url_set.find(test_url + std::to_string(i)));
 						openedItemsCount += 1;
 					}
 				}
@@ -108,15 +114,17 @@ TEST_CASE("itemlist_formaction::process_op()") {
 		SECTION("Open all unread in browser and mark read, unread < max-browser-tabs"){
 			int maxItemsToOpen = 9;
 			int openedItemsCount = 0;
-			cfg->set_configvalue("max-browser-tabs", std::to_string(maxItemsToOpen));
+			cfg.set_configvalue("max-browser-tabs", std::to_string(maxItemsToOpen));
 
-			itemlist->process_op(OP_OPENALLUNREADINBROWSER_AND_MARK);
+			itemlist.process_op(OP_OPENALLUNREADINBROWSER_AND_MARK);
 
 			std::ifstream browserFileStream (browserfile.getPath());
 			if (browserFileStream.is_open()) {
 				for (int i = 0; i < itemCount; i ++) {
 					if ( std::getline (browserFileStream,line) ) {
-						REQUIRE(line == test_url + std::to_string(i));
+						INFO("Each URL should be present exactly once. Erase urls after first match to fail if an item opens twice.")
+						REQUIRE(url_set.count(test_url + std::to_string(i)) == 1);
+						url_set.erase(url_set.find(test_url + std::to_string(i)));
 						openedItemsCount += 1;
 					}
 				}
