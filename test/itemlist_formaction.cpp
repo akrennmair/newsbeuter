35a39b043153fccc0e06216498cbb3d79624d738
tanguy <tanguy@uboat>
2016-11-12 16:30:30 +0100

    Improve coverage of itemlist_formaction test
    This adds support for all commands available from the itemlist
    formaction to its test class.

--- test/itemlist_formaction.cpp
+++ test/itemlist_formaction.cpp
@@ -16,7 +16,20 @@ TEST_CASE("process_op()", "[itemlist_formaction]") {
 	TestHelpers::TempFile browserfile;
 	std::unordered_set<std::string> url_set;
 
+	std::vector<std::shared_ptr<rss_feed>> feedlist;
+
 	std::string test_url = "http://test_url";
+	std::string test_title = "Article Title";
+	std::string test_author = "Article Author";
+	std::string test_description = "Article Description";
+	time_t test_pubDate = 42;
+	std::string test_puDate_str = "Thu, 01 Jan 1970 01:00:42 +0100";
+
+	std::string pager_prefix_title = "Title: ";
+	std::string pager_prefix_author = "Author: ";
+	std::string pager_prefix_date = "Date: ";
+	std::string pager_prefix_link = "Link: ";
+
 	std::string line;
 	int itemCount = 6;
 
@@ -30,6 +43,10 @@ TEST_CASE("process_op()", "[itemlist_formaction]") {
 	for (int i = 0; i < itemCount; i++) {
 		std::shared_ptr<rss_item> item = std::make_shared<rss_item>(&rsscache);
 		item->set_link(test_url + std::to_string(i));
+		item->set_title(test_title);
+		item->set_author(test_author);
+		item->set_description(test_description);
+		item->set_pubDate(test_pubDate);
 		url_set.insert(test_url + std::to_string(i));
 		item->set_unread(true);
 		feed->add_item(item);
@@ -41,7 +58,65 @@ TEST_CASE("process_op()", "[itemlist_formaction]") {
 	itemlist_formaction itemlist(&v, itemlist_str);
 	itemlist.set_feed(feed);
 
-	SECTION("Itemlist Formaction"){
+	SECTION("OP_OPEN") {
+		std::string articleLine;
+		TestHelpers::TempFile pagerfile;
+		cfg.set_configvalue("pager", "cat %f > " + pagerfile.getPath());
+
+		REQUIRE_NOTHROW(itemlist.process_op(OP_OPEN));
+
+		std::ifstream pagerFileStream (pagerfile.getPath());
+		REQUIRE( std::getline (pagerFileStream,articleLine) );
+		REQUIRE(articleLine == pager_prefix_title + test_title);
+
+		REQUIRE( std::getline (pagerFileStream,articleLine) );
+		REQUIRE(articleLine == pager_prefix_author + test_author);
+
+		REQUIRE( std::getline (pagerFileStream,articleLine) );
+		REQUIRE(articleLine == pager_prefix_date + test_puDate_str);
+
+		REQUIRE( std::getline (pagerFileStream,articleLine) );
+		REQUIRE(articleLine.find(pager_prefix_link + test_url) == 0);
+
+		REQUIRE( std::getline (pagerFileStream,articleLine) );
+		REQUIRE(articleLine == " ");
+
+		REQUIRE( std::getline (pagerFileStream,articleLine) );
+		REQUIRE(articleLine == test_description);
+
+		REQUIRE( std::getline (pagerFileStream,articleLine) );
+		REQUIRE(articleLine == "");
+
+		pagerFileStream.close();
+	}
+	SECTION("OP_DELETE") {
+		//REQUIRE_NOTHROW(itemlist.process_op(OP_DELETE));
+		//REQUIRE(feed->total_item_count() == itemCount -1);
+		//Crash, to investigate
+	}
+	SECTION("OP_PURGE_DELETED") {
+		REQUIRE_NOTHROW(itemlist.process_op(OP_PURGE_DELETED));
+		//Trigger deletion before in order to test properly...
+	}
+	SECTION("OP_OPENBROWSER_AND_MARK") {
+		itemlist.process_op(OP_OPENBROWSER_AND_MARK);
+		std::ifstream browserFileStream (browserfile.getPath());
+
+		REQUIRE ( std::getline (browserFileStream,line) );
+		REQUIRE(line.find(test_url) == 0);
+		browserFileStream.close();
+
+		REQUIRE(feed->unread_item_count() == itemCount - 1);
+	}
+	SECTION("OP_OPENINBROWSER") {
+		itemlist.process_op(OP_OPENINBROWSER);
+		std::ifstream browserFileStream (browserfile.getPath());
+
+		REQUIRE ( std::getline (browserFileStream,line) );
+		REQUIRE(line.find(test_url) == 0);
+		browserFileStream.close();
+	}
+	SECTION("OP_OPENALLUNREADINBROWSER"){
 	
 		SECTION("Open all unread in browser, unread >= max-browser-tabs"){
 			int maxItemsToOpen = 4;
@@ -83,6 +158,8 @@ TEST_CASE("process_op()", "[itemlist_formaction]") {
 			}
 			REQUIRE(openedItemsCount == itemCount);
 		}
+	}
+	SECTION("OP_OPENALLUNREADINBROWSER_AND_MARK"){
 
 		SECTION("Open all unread in browser and mark read, unread >= max-browser-tabs"){
 			int maxItemsToOpen = 4;
@@ -125,6 +202,105 @@ TEST_CASE("process_op()", "[itemlist_formaction]") {
 			REQUIRE(openedItemsCount == itemCount);
 			REQUIRE(feed->unread_item_count() == 0);
 		}
+	}
+	SECTION("OP_TOGGLEITEMREAD") {
+		//NOTIMPL
+	}
+	SECTION("OP_SHOWURLS") {
+		//NOTIMPL
+	}
+	SECTION("OP_BOOKMARK") {
+		//NOTIMPL
+	}
+	SECTION("OP_EDITFLAGS") {
+		//NOTIMPL
+	}
+	SECTION("OP_SAVE") {
+		//NOTIMPL
+	}
+	SECTION("OP_HELP") {
+		//NOTIMPL
+	}
+	SECTION("OP_RELOAD") {
+		//NOTIMPL
+	}
+	SECTION("OP_QUIT") {
+		//NOTIMPL
+	}
+	SECTION("OP_HARDQUIT") {
+		//NOTIMPL
+	}
+	SECTION("OP_NEXTUNREAD") {
+		//NOTIMPL
+	}
+	SECTION("OP_PREVUNREAD") {
+		//NOTIMPL
+	}
+	SECTION("OP_NEXT") {
+		//NOTIMPL
+	}
+	SECTION("OP_PREV") {
+		//NOTIMPL
+	}
+	SECTION("OP_RANDOMUNREAD") {
+		//NOTIMPL
+	}
+	SECTION("OP_NEXTUNREADFEED") {
+		//NOTIMPL
+	}
+	SECTION("OP_PREVUNREADFEED") {
+		//NOTIMPL
+	}
+	SECTION("OP_NEXTFEED") {
+		//NOTIMPL
+	}
+	SECTION("OP_PREVFEED") {
+		//NOTIMPL
+	}
+	SECTION("OP_MARKFEEDREAD") {
+		//NOTIMPL
+	}
+	SECTION("OP_TOGGLESHOWREAD") {
+		//NOTIMPL
+	}
+	SECTION("OP_PIPE_TO") {
+		//NOTIMPL
+	}
+	SECTION("OP_SEARCH") {
+		//NOTIMPL
+	}
+	SECTION("OP_EDIT_URLS") {
+		//NOTIMPL
+	}
+	SECTION("OP_SELECTFILTER") {
+		//NOTIMPL
+	}
+	SECTION("OP_SETFILTER") {
+		//NOTIMPL
+	}
+	SECTION("OP_CLEARFILTER") {
+		//NOTIMPL
+	}
+	SECTION("OP_SORT") {
+		//NOTIMPL
+	}
+	SECTION("OP_REVSORT") {
+		//NOTIMPL
+	}
+	SECTION("OP_INT_RESIZE") {
+		//NOTIMPL
+	}
+	SECTION("OP_INT_END_SETFILTER") {
+		//NOTIMPL
+	}
+	SECTION("OP_INT_EDITFLAGS_END") {
+		//NOTIMPL
+	}
+	SECTION("OP_INT_START_SEARCH") {
+		//NOTIMPL
+	}
+	SECTION("OP_PIPE_TO") {
+		//NOTIMPL
 	}
 
 }
